454b531c34a14c99fd5045bb01c8813654707c87
Guillaume DUCHON <g.duchon@agencesurf.com>
2021-01-04 15:49:57 +0100

    màj CSS/IMG + Mecanique Splashscreen

--- server/magasin.php
+++ server/magasin.php
@@ -1,7 +1,7 @@
 <?php
     require_once 'config.php';
     
-    $stmt = $pdo->query("SELECT * FROM question");
+    $stmt = $pdo->query("SELECT * FROM magasin");
     $user = $stmt->fetch();
 
     return  print json_encode($user);
