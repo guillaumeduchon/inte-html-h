159010cb1a9b4f216bea256e8bbb4461c50c3a6c
Lory LETICEE <l.leticee@agencesurf.com>
2021-01-04 16:50:35 +0100

    ok lgin and list magasin

--- server/magasin.php
+++ server/magasin.php
@@ -1,9 +1,7 @@
 <?php
     require_once 'config.php';
     
-    $stmt = $pdo->query("SELECT * FROM question");
-    $user = $stmt->fetch();
-
-    return  print json_encode($user);
-
+    $stmt = $pdo->query("SELECT * FROM magasin");
+    $magasins = $stmt->fetchAll();
+    return  print json_encode($magasins);
 ?>
