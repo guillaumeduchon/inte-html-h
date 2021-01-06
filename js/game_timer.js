0f6670d98f467b0e4373e63b8b622d362c824640
Lory LÉTICÉE <loryleticee@gmail.com>
2021-01-06 21:46:28 +0100

    add

--- js/game_timer.js
+++ js/game_timer.js
@@ -57,8 +57,7 @@ function onTimesUp() {
 
 function startTimer() {
   timerInterval = setInterval(() => {
-    console.log('TEST :', Number(localStorage.getItem('timePassed'))+1)
-    timePassed = localStorage.getItem('timePassed') += 1;
+    timePassed = Number(localStorage.getItem('timePassed')) + 1;
     localStorage.setItem('timePassed', timePassed);
     timeLeft = TIME_LIMIT - timePassed;
     localStorage.setItem('timeLeft',timeLeft)
