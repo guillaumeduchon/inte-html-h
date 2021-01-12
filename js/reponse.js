a81128281e545a6530c17a600998db477c74b573
Lory LÉTICÉE <loryleticee@gmail.com>
2021-01-12 23:54:36 +0100

    ..

--- js/reponse.js
+++ js/reponse.js
@@ -1,17 +1,6 @@
 // create context object
 var context = {};
 
-// assign functions to the object
-context["check_answer1"] = check_answer1;
-context["check_answer2"] = check_answer2;
-context["check_answer3"] = check_answer3;
-context["check_answer4"] = check_answer4;
-context["check_answer5"] = check_answer5;
-context["check_answer6"] = check_answer6;
-context["check_answer7"] = check_answer7;
-context["check_answer8"] = check_answer8;
-context["check_answer9"] = check_answer9;
-
 /* ----------------------------------- REPONSE JEU 1 ----------------------------------- */
 const fetch_reponse = async () => {
   await axios.post('/server/reponse.php', { day_num: DAY_NUM }, {
@@ -26,10 +15,6 @@ const fetch_reponse = async () => {
         showError();
       }
     });
-}
-
-const check_answer = () => {
-  context["check_answer"+DAY_NUM].apply()
 }
 
 const check_answer1 = () => {
@@ -587,4 +572,20 @@ function colors(tableauTri, game6) {
   } else {
     colors_button2(tableauTri);
   }
+}
+
+
+// assign functions to the object
+context["check_answer1"] = check_answer1;
+context["check_answer2"] = check_answer2;
+context["check_answer3"] = check_answer3;
+context["check_answer4"] = check_answer4;
+context["check_answer5"] = check_answer5;
+context["check_answer6"] = check_answer6;
+context["check_answer7"] = check_answer7;
+context["check_answer8"] = check_answer8;
+context["check_answer9"] = check_answer9;
+
+const check_answer = () => {
+  context["check_answer"+DAY_NUM].apply()
 }
