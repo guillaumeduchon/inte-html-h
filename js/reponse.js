2af7e15fc774db7f64b90e8ac9b07e5e0a3e8799
Lory LÉTICÉE <loryleticee@gmail.com>
2021-01-12 22:35:14 +0100

    ..

--- js/reponse.js
+++ js/reponse.js
@@ -337,8 +337,8 @@ const fetch_reponse_valid6 = async () => {
 
         var existFalseAnswer = false;
         $('.dropzone > .dropdiv.dz').each((index, el) => {
-          let rowImgId = 000;
-          if ($(el).hasClass('.answer_img')) {
+          let rowImgId = 100;
+          if ($(el).children().hasClass('.answer_img')) {
             rowImgId = getId($(el).attr('id'));
           }
           console.log('TEST :', rowImgId ,tableauTriJ6[index].id)
