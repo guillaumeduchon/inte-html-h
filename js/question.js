99cb053dbed9b637bff9d7f4294934afc4361633
Guillaume Duchon <guillaumeduchon@icloud.com>
2021-01-20 14:22:23 +0100

    correct

--- js/question.js
+++ js/question.js
@@ -9,9 +9,13 @@ const fetch_rules = async () => {
         $('.quizz').find('strong').html(`Jour ${resp.data.jour}`)
         $('.rulegame').html(`<p>${resp.data.rules}</p>`)
         if (resp.data.type_game === 'drag') {
-          $('.explanation').html(`<img src="img/icon_drag_drop.png" alt="Drag and drop"><p>Glisser &amp; déposer<br>Glissez dans la zone dédiée</p>`)
-        } else if (resp.data.type_game === 'quiz') {
+          $('.explanation').html(`<img src="img/icon_drag_drop.png" alt="Drag and drop"><p>Drag &amp; drop<br>Glissez et déposez dans la zone dédiée</p>`)
+        } else if (resp.data.type_game === 'quiz1') {
           $('.explanation').html(`<img src="img/icon_quiz.png" alt="Quiz"><p>Quiz<br>Sélectionnez les bonnes réponses</p>`)
+        } else if (resp.data.type_game === 'quiz2') {
+          $('.explanation').html(`<img src="img/icon_quiz.png" alt="Quiz"><p>Quiz<br>Sélectionnez la bonne réponse</p>`)
+        } else if (resp.data.type_game === 'quiz3') {
+          $('.explanation').html(`<img src="img/icon_quiz.png" alt="Quiz"><p>Quiz<br>Trouvez la bonne réponse</p>`)
         } else if (resp.data.type_game === 'swipe') {
           $('.explanation').html(`<img src="img/icon_swipe.png" alt="Swipe"><p>Swipe<br>Choisissez en faisant défiler les images</p>`)
         }
