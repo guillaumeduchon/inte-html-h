59c72241c98fe4936a9854b9f11338909bfee42c
Lory LETICEE <l.leticee@agencesurf.com>
2021-01-21 13:44:09 +0100

    compte à rebours ok pour 10h24

--- js/custom.js
+++ js/custom.js
@@ -5,35 +5,38 @@ $(document).on('click', '.carousel_cell-content-linkgame', (el) => {
 
 
 const DATE_TAB = [
-  { 1: '21/01/2021' },
-  { 2: '21/01/2021' },
-  { 3: '21/01/2021' },
-  { 4: '21/01/2021' },
-  { 5: '21/01/2021' },
-  { 6: '21/01/2021' },
-  { 7: '21/01/2021' },
-  { 8: '21/01/2021' },
-  { 9: '21/01/2021' },
-  { 10: '21/01/2021' }
+  { 1: '2021/01/21' },
+  { 2: '2021/01/22' },
+  { 3: '2021/01/27' },
+  { 4: '2021/01/28' },
+  { 5: '2021/01/29' },
+  { 6: '2021/01/30' },
+  { 7: '2021/02/01' },
+  { 8: '2021/02/02' },
+  { 9: '2021/02/03' },
+  { 10: '2021/02/04' }
 ];
 var date_today = get_date_today(new Date())
-var tab_day = Object.keys(DATE_TAB.filter(obj=>( Object.values(obj) == date_today))[0])
-const DAY_NUM = tab_day[0];
+
+if(date_today >= DATE_TAB[0]) {
+  var tab_day = Object.keys(DATE_TAB.filter(obj=>( Object.values(obj) == date_today))[0])
+  const DAY_NUM = tab_day[0];
+}
 
 //------------------------------------------------PLATEAU---------------------------------------
 
 const updatePlateau = () => {
   let date_tab = [
-    {'status':'','day_num': 1, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 2, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 3, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 4, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 5, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 6, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 7, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 8, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 9, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 10, 'day_date':'21/01/2021'},
+    {'status':'','day_num': 1, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 2, 'day_date':'2021/01/22'},
+    {'status':'','day_num': 3, 'day_date':'2021/01/27'},
+    {'status':'','day_num': 4, 'day_date':'2021/01/28'},
+    {'status':'','day_num': 5, 'day_date':'2021/01/29'},
+    {'status':'','day_num': 6, 'day_date':'2021/01/30'},
+    {'status':'','day_num': 7, 'day_date':'2021/02/01'},
+    {'status':'','day_num': 8, 'day_date':'2021/02/02'},
+    {'status':'','day_num': 9, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 10, 'day_date':'2021/02/04'},
   ];
 
   let today = new Date();
@@ -41,7 +44,7 @@ const updatePlateau = () => {
   let MONTH = (montRaw.length < 2 ? '0' + montRaw : montRaw);
   let dayRaw = String(today.getUTCDate());
   let DAY = (dayRaw.length < 2 ? '0' + dayRaw : dayRaw);
-  let today_date = `${DAY}/${MONTH}/${today.getFullYear()}`;
+  let today_date = `${today.getFullYear()}/${MONTH}/${DAY}`;
 
   var $carousel = $('.carousel_plateau').flickity();
   // console.log($carousel);
@@ -52,11 +55,11 @@ const updatePlateau = () => {
       el.status = 'available';
       el.img = 'img/fond_plateau_available.png';
       el.iconDisplay = 'hide';
-      el.linkDisplay = '';
+      el.linkDisplay = '';  
       var indexSlide = tab_day-1;
       $carousel.flickity('select', indexSlide);
     }
-    if (el.day_date > today_date) {
+    if ((el.day_date > today_date)) {
       el.highlight = '';
       el.status = 'unavailable';
       el.img = 'img/fond_plateau_unavailable.png';
@@ -142,38 +145,39 @@ function clear_counter(){
 function compte_a_rebours(){
   var date_actuelle = new Date();
   const date_evenement = new Date(date_actuelle)
-  date_evenement.setDate(date_evenement.getDate() + 1)
-  date_evenement.setHours(00, 00, 00);
+  date_evenement.setDate(date_evenement.getDate()+1)
+  date_evenement.setHours(10, 24, 00);
   var total_secondes = (date_evenement - date_actuelle) / 1000;
-  
   var jours = new Array("Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi");
   var heures = Math.floor((total_secondes - (0 * 60 * 60 * 24)) / (60 * 60));
   var minutes = Math.floor((total_secondes - ((0 * 60 * 60 * 24 + heures * 60 * 60))) / 60);
   var secondes = Math.floor(total_secondes - ((0 * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
 
-  $('.countdown').find('strong').html(`${heures} H ${minutes} MIN ${secondes} S`);
+  $('.countdown').find('strong').html(`${Math.abs(heures)} H ${minutes} MIN ${secondes} S`);
   $('.unavailable:eq(0)').find('.statut').html(`<img class="icon" src="img/icon_cadenas.png" alt="">Disponible dans<br><strong>${heures} H ${minutes} MIN ${secondes} S</strong>`);
 
   $('.expired').find('.statut').html('Challenge terminé');
   $('.available').find('.statut').addClass('countdown');
-  $('.countdown').html(`Il vous reste encore<br><strong>${heures} H ${minutes} MIN ${secondes} S</strong><br>pour trouver l\'indice du jour`);
+  $('.countdown').html(`Il vous reste encore<br><strong>${Math.abs(heures)} H ${minutes} MIN ${secondes} S</strong><br>pour trouver l\'indice du jour`);
   $('.unavailable').each((index, el)=>{
     if (index === 0){
-      $(el).find('.statut').html(`<img class="icon" src="img/icon_cadenas.png" alt="">Disponible dans<br><strong> ${heures} H ${minutes} MIN ${secondes} S</strong>`);
+      $(el).find('.statut').html(`<img class="icon" src="img/icon_cadenas.png" alt="">Disponible dans<br><strong> ${Math.abs(heures)} H ${minutes} MIN ${secondes} S</strong>`);
     } else {
       //Si le prochain jours est Dimanche
-      var joursWeekEnd ;
-      if (jours[date_evenement.getDay()+index] === undefined) {
+      var joursSuivant ;
+      if (jours[date_evenement.getDay() + index] === undefined) {
         //Je creer un tableau contenant uniquement les jours de lundi à  samedi
         let jours_ouvre = jours.slice(1);
-        joursWeekEnd = jours_ouvre[(index -2)]
-        joursWeekEnd = joursWeekEnd === "Dimanche" ?  joursWeekEnd = 'Lundi': joursWeekEnd;
+  
+        if(index > 7) { index = 2 + index - 8 }
+
+        joursSuivant = jours_ouvre[(index -2)];
 
       } else {
-        joursWeekEnd= jours[date_evenement.getDay()+index]
+        joursSuivant= jours[date_evenement.getDay() + index]
       } 
 
-      $(el).find('.statut').html(`<img class="icon" src="img/icon_cadenas.png" alt="">Disponible<br><strong>${joursWeekEnd}</strong>`);
+      $(el).find('.statut').html(`<img class="icon" src="img/icon_cadenas.png" alt="">Disponible<br><strong>${joursSuivant}</strong>`);
     }
   })
   var actualisation = setTimeout("compte_a_rebours();", 1000);
@@ -186,5 +190,5 @@ function get_date_today(d) {
   let dayRaw = String(today.getUTCDate());
   let DAY = (dayRaw.length < 2 ? '0' + dayRaw : dayRaw);
 
-  return `${DAY}/${MONTH}/${today.getFullYear()}`; 
+  return `${today.getFullYear()}/${MONTH}/${DAY}`; 
 }
