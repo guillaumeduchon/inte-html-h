27af88ea654da24ba6ccc429a15c8c9fb0cb0860
Lory LETICEE <l.leticee@agencesurf.com>
2021-01-22 11:07:32 +0100

    date US

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 const DATE_TAB = [
-  { 1: '20/01/2021' },
-  { 2: '18/01/2021' },
-  { 3: '19/01/2021' },
-  { 4: '17/01/2021' },
-  { 5: '21/01/2021' },
-  { 6: '22/01/2021' },
-  { 7: '23/01/2021' },
-  { 8: '24/01/2021' },
-  { 9: '25/01/2021' },
-  { 10: '26/01/2021' }
+  { 1: '2021/01/21' },
+  { 2: '2021/01/21' },
+  { 3: '2021/01/21' },
+  { 4: '2021/01/21' }, 
+  { 5: '2021/01/21' },
+  { 6: '2021/01/21' },
+  { 7: '2021/01/21' },
+  { 8: '2021/01/21' },
+  { 9: '2021/01/21' },
+  { 10: '2021/01/21' }
 ];
 var date_today = get_date_today(new Date())
 var tab_day = Object.keys(DATE_TAB.filter(obj=>( Object.values(obj) == date_today))[0])
@@ -18,16 +18,16 @@ const DAY_NUM = tab_day[0];
 
 const updatePlateau = () => {
   let date_tab = [
-    {'status':'','day_num': 1, 'day_date':'20/01/2021'},
-    {'status':'','day_num': 2, 'day_date':'18/01/2021'},
-    {'status':'','day_num': 3, 'day_date':'19/01/2021'},
-    {'status':'','day_num': 4, 'day_date':'17/01/2021'},
-    {'status':'','day_num': 5, 'day_date':'21/01/2021'},
-    {'status':'','day_num': 6, 'day_date':'22/01/2021'},
-    {'status':'','day_num': 7, 'day_date':'23/01/2021'},
-    {'status':'','day_num': 8, 'day_date':'24/01/2021'},
-    {'status':'','day_num': 9, 'day_date':'25/01/2021'},
-    {'status':'','day_num': 10, 'day_date':'26/01/2021'},
+    {'status':'','day_num': 1, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 2, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 3, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 4, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 5, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 6, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 7, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 8, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 9, 'day_date':'2021/01/21'},
+    {'status':'','day_num': 10, 'day_date':'2021/01/21'},
   ];
 
   let today = new Date();
@@ -35,7 +35,7 @@ const updatePlateau = () => {
   let MONTH = (montRaw.length < 2 ? '0' + montRaw : montRaw);
   let dayRaw = String(today.getUTCDate());
   let DAY = (dayRaw.length < 2 ? '0' + dayRaw : dayRaw);
-  let today_date = `${DAY}/${MONTH}/${today.getFullYear()}`;
+  let today_date = `${today.getFullYear()}/${MONTH}/${DAY}`;
 
   var $carousel = $('.carousel_plateau').flickity();
 
@@ -161,5 +161,5 @@ function get_date_today(d) {
   let dayRaw = String(today.getUTCDate());
   let DAY = (dayRaw.length < 2 ? '0' + dayRaw : dayRaw);
 
-  return `${DAY}/${MONTH}/${today.getFullYear()}`; 
+  return `${today.getFullYear()}/${MONTH}/${DAY}`; 
 }
