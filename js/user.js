2dc186d765a4316acee436d37edce0ed3151ac1d
Guillaume Duchon <guillaumeduchon@icloud.com>
2021-01-25 15:21:06 +0100

    checkbox login legal

--- js/user.js
+++ js/user.js
@@ -19,11 +19,11 @@ function disconnect() {
 const fetch_login = (e) => {
   var code = document.getElementById("code").value;
   var magasin = document.getElementById("magasin").value;
-  var acceptGame = document.getElementById("acceptGame");
-  var acceptData = document.getElementById("acceptData");
+  var acceptGame = document.getElementById("acceptGame").checked;
+  var acceptData = document.getElementById("acceptData").checked;
   if(magasin !== '') {
     // try_login(magasin, code);
-    if ((acceptGame === true) && (acceptData === true)) {
+    if ((acceptGame == true) && (acceptData == true)) {
       try_login(magasin, code);
     } else {
       showConsent();
