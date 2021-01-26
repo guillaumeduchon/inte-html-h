3da757a165d41e7eb59311e00e3322602a3bf761
Guillaume Duchon <guillaumeduchon@icloud.com>
2021-01-26 18:03:35 +0100

    cookies

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 const DATE_TAB = [
-  { 1: '2021/01/23' },
-  { 2: '2021/01/24' },
-  { 3: '2021/01/25' },
-  { 4: '2021/01/26' },
-  { 5: '2021/01/29' },
-  { 6: '2021/01/30' },
-  { 7: '2021/01/31' },
-  { 8: '2021/02/01' },
-  { 9: '2021/02/02' },
-  { 10: '2021/02/03' }
+  { 1: '2021/01/22' },
+  { 2: '2021/01/23' },
+  { 3: '2021/01/24' },
+  { 4: '2021/01/25' },
+  { 5: '2021/01/26' },
+  { 6: '2021/01/27' },
+  { 7: '2021/01/28' },
+  { 8: '2021/01/29' },
+  { 9: '2021/01/30' },
+  { 10: '2021/01/31' }
 ];
 var date_today = get_date_today(new Date())
 var tab_day = Object.keys(DATE_TAB.filter(obj => (Object.values(obj) == date_today))[0])
@@ -18,16 +18,16 @@ localStorage.setItem('DAY_NUM', tab_day[0]);
 
 const updatePlateau = () => {
   let date_tab = [
-    { 'status': '', 'day_num': 1, 'day_date': '2021/01/23' },
-    { 'status': '', 'day_num': 2, 'day_date': '2021/01/24' },
-    { 'status': '', 'day_num': 3, 'day_date': '2021/01/25' },
-    { 'status': '', 'day_num': 4, 'day_date': '2021/01/26' },
-    { 'status': '', 'day_num': 5, 'day_date': '2021/01/29' },
-    { 'status': '', 'day_num': 6, 'day_date': '2021/01/30' },
-    { 'status': '', 'day_num': 7, 'day_date': '2021/01/31' },
-    { 'status': '', 'day_num': 8, 'day_date': '2021/02/01' },
-    { 'status': '', 'day_num': 9, 'day_date': '2021/02/02' },
-    { 'status': '', 'day_num': 10, 'day_date': '2021/02/03' },
+    { 'status': '', 'day_num': 1, 'day_date': '2021/01/22' },
+    { 'status': '', 'day_num': 2, 'day_date': '2021/01/23' },
+    { 'status': '', 'day_num': 3, 'day_date': '2021/01/24' },
+    { 'status': '', 'day_num': 4, 'day_date': '2021/01/25' },
+    { 'status': '', 'day_num': 5, 'day_date': '2021/01/26' },
+    { 'status': '', 'day_num': 6, 'day_date': '2021/01/27' },
+    { 'status': '', 'day_num': 7, 'day_date': '2021/01/28' },
+    { 'status': '', 'day_num': 8, 'day_date': '2021/01/29' },
+    { 'status': '', 'day_num': 9, 'day_date': '2021/01/30' },
+    { 'status': '', 'day_num': 10, 'day_date': '2021/01/31' },
   ];
 
   let today = new Date();
@@ -103,6 +103,17 @@ function showError() {
   $('.wrongId').attr('style', 'display:block');
   setTimeout(function () {
     $('.wrongId').attr('style', 'display:none');
+  }, 6000);
+}
+
+function hideConsent() {
+  $('.wrongConsent').attr('style', 'display:none');
+}
+
+function showConsent() {
+  $('.wrongConsent').attr('style', 'display:block');
+  setTimeout(function () {
+    $('.wrongConsent').attr('style', 'display:none');
   }, 6000);
 }
 
@@ -228,7 +239,7 @@ const ShowGamePlayed = () => {
         JSON.parse(localStorage.getItem('game_played')).forEach((game, i) => {
           if ((index + 1) === game.id) {
             if (game.indice_id > 0) {
-              $('.available:eq(' + (game.id - 1) + ')').find('.statut.countdown').html('Challenge gagné<br>Cliquez sur jouer pour<br>voir l&lsquo;indice');
+              $('.expired:eq(' + (game.id - 1) + ')').find('.statut').html('Challenge gagné');
             } else {
               $('.expired:eq(' + (game.id - 1) + ')').find('.statut').html('Challenge perdu');
             }
