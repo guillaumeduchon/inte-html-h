d50683ca6e956b69e1050a18027884adaf26f257
Lory LÉTICÉE <loryleticee@gmail.com>
2021-01-29 13:08:57 +0100

    prepare dotenv with comment

--- server/config.php
+++ server/config.php
@@ -1,4 +1,7 @@
 <?php
+
+    require_once 'dotenv.php';
+
     /**
      * Config
      * Author: Agence Surf
@@ -15,12 +18,18 @@
     // $port = "3306";
     // $charset = 'utf8mb4';
 
-    $host = '127.0.0.1';
-    $db   = 'jeu_hermes';
-    $user = 'root';
-    $pass = '';
-    $port = "3306";
-    $charset = 'utf8mb4';
+    // $_ENV = new DotEnv(__DIR__ . '/.env')->load();
+
+    echo getenv('APP_ENV');
+    // dev
+    echo getenv('DATABASE_DNS');
+
+    $host = '127.0.0.1'; //$_ENV->getenv('APP_HOST')
+    $db   = 'jeu_hermes'; //$_ENV->getenv('APP_BDD')
+    $user = 'root'; //$_ENV->getenv('APP_USER')
+    $pass = ''; //$_ENV->getenv('APP_PASS')
+    $port = "3306"; //$_ENV->getenv('APP_PORT')
+    $charset = 'utf8mb4'; //$_ENV->getenv('APP_CHARSET')
 
     $options = [
         \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
