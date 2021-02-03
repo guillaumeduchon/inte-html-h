6b1e7baf80d39314ef82588ef20e9a0bcabdc5bb
Guillaume Duchon <guillaumeduchon@icloud.com>
2021-02-03 10:39:43 +0100

    modif drag

--- js/reponse.js
+++ js/reponse.js
@@ -624,16 +624,12 @@ function colors_button2(tableauTri, idsTab1, idsTab2) {
   $('.answer_button').each((index, button) => {
     console.log('INDEX:', index);
     if(index < 2) {
-      if (!idsTab1.includes(getId($(index).attr('id')))) {
+      if (!idsTab1.includes(getId($(button).attr('id')))) {
         tableauTri[index].id === getId($(button).attr('id')) ? $(button).addClass('win') : $(button).addClass('lose')
-      }else {
-        $(button).addClass('lose')
       }
     } else {
-      if (!idsTab2.includes(getId($(index).attr('id')))) {
+      if (!idsTab2.includes(getId($(button).attr('id')))) {
         tableauTri[index].id === getId($(button).attr('id')) ? $(button).addClass('win') : $(button).addClass('lose')
-      } else {
-        $(button).addClass('lose')
       }
     }
   })
