04ae97fb6b09585ea627e364f248d0699df064b6
Guillaume Duchon <guillaumeduchon@icloud.com>
2021-02-03 10:05:01 +0100

    correct 3 fev

--- js/custom.js
+++ js/custom.js
@@ -4,16 +4,16 @@ $(document).on('click', '.carousel_cell-content-linkgame', (el) => {
 })
 
 const DATE_TAB = [
-  { 1: '2021/02/02' },
-  { 2: '2021/02/02' },
-  { 3: '2021/02/02' },
-  { 4: '2021/02/02' }, 
-  { 5: '2021/02/02' },
-  { 6: '2021/02/02' },
-  { 7: '2021/02/02' },
-  { 8: '2021/02/02' },
-  { 9: '2021/02/02' },
-  { 10: '2021/02/02' }
+  { 1: '2021/02/03' },
+  { 2: '2021/02/03' },
+  { 3: '2021/02/03' },
+  { 4: '2021/02/03' }, 
+  { 5: '2021/02/03' },
+  { 6: '2021/02/03' },
+  { 7: '2021/02/03' },
+  { 8: '2021/02/03' },
+  { 9: '2021/02/03' },
+  { 10: '2021/02/03' }
 ];
 
 var date_today = get_date_today(new Date())
@@ -27,16 +27,16 @@ if(date_today >= Object.values(DATE_TAB[0])[0]) {
 
 const updatePlateau = () => {
   let date_tab = [
-    {'status':'','day_num': 1, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 2, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 3, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 4, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 5, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 6, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 7, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 8, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 9, 'day_date':'2021/02/02'},
-    {'status':'','day_num': 10, 'day_date':'2021/02/02'},
+    {'status':'','day_num': 1, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 2, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 3, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 4, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 5, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 6, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 7, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 8, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 9, 'day_date':'2021/02/03'},
+    {'status':'','day_num': 10, 'day_date':'2021/02/03'},
   ]
 
   let today = new Date();
