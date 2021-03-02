e57689f34115739f47651e0e43ac33e01109d04f
Guillaume Duchon <guillaumeduchon@icloud.com>
2021-03-02 16:29:52 +0100

    reload

--- js/custom.js
+++ js/custom.js
@@ -214,8 +214,8 @@ localStorage.setItem('nbInBefore10h24', 0);
 
 function before10h24(heures, minutes, secondes) {
   if (heures === 24 && minutes === 00 && secondes === 00) {
-    location.reload();
     localStorage.removeItem('DAY_NUM_BEFORE');
+    location.reload();
   }
 
   if (heures >= 24 ) {
