258907ed4abaeb1b2cd91e09dde16f3325173871
lory leticee <loryleticee@gmail.com>
2021-05-25 14:11:51 +0200

    add if date server exist

--- js/custom.js
+++ js/custom.js
@@ -9,16 +9,18 @@ function check_date_is_paris() {
   server_date = server_date.getDay() + '/' + server_date.getUTCMonth() + '/' + server_date.getHours();
   client_date = client_date.getDay() + '/' + client_date.getUTCMonth() + '/' + client_date.getHours();
 
-
-  console.log('server_date: ', server_date, "client_date", client_date)
-  if (server_date !== client_date) {
+  if(localStorage.getItem('DATE_SERVER') !== null) {
     console.log('server_date: ', server_date, "client_date", client_date)
-    window.location.href = "not_good_date.html";
-  }
-  date_first_game = new Date('2021/05/24');
-  console.log('server_date_copy: ',server_date_copy, 'date_first_game', date_first_game );
-  if(server_date_copy < date_first_game) {
-    window.location.href = "not_open.html";
+    if (server_date !== client_date) {
+      console.log('server_date: ', server_date, "client_date", client_date)
+      window.location.href = "not_good_date.html";
+    }
+    date_first_game = new Date('2021/05/24');
+    console.log('server_date_copy: ',server_date_copy, 'date_first_game', date_first_game );
+  
+    if(server_date_copy < date_first_game) {
+      window.location.href = "not_open.html";
+    }
   }
 }
 
