c4d4b932e6733f6c2391258ed86b59dd14b17555
loryleticee <loryleticee@gmail.com>
2021-08-18 17:16:16 +0200

    remove timer and add form multiple file for game 2 and game 4

--- js/reponse.js
+++ js/reponse.js
@@ -11,15 +11,14 @@ const check_answer1 = async () => {
 /* ----------------------------------- REPONSE JEU 2 ----------------------------------- */
 
 const check_answer2 = () => {
-
-  //HERE SAVE FILE
-
-  await axios.post('/server/set_reponse.php', { day_num: '2', response: document.getElementById('answer').value, magasin_num: localStorage.getItem('magasin') }, {
-    headers: { 'Content-Type': 'application/json', 'mode': 'cors' }
-  })
-    .then((valid_resp) => {
-      alert(valid_resp)
-    });
+  saveFiles().then(function () {
+    await axios.post('/server/set_reponse.php', { day_num: '2', response: document.getElementById('answer').value, magasin_num: localStorage.getItem('magasin') }, {
+      headers: { 'Content-Type': 'application/json', 'mode': 'cors' }
+    })
+      .then((valid_resp) => {
+        alert(valid_resp)
+      });
+  });
 }
 
 /* ----------------------------------- REPONSE JEU 3 ----------------------------------- */
@@ -35,13 +34,14 @@ const check_answer3 = async () => {
 
 const check_answer4 = () => {
   //HERE SAVE FILE
-
-  await axios.post('/server/set_reponse.php', { day_num: '4', response: document.getElementById('answer').value, magasin_num: localStorage.getItem('magasin') }, {
-    headers: { 'Content-Type': 'application/json', 'mode': 'cors' }
-  })
-    .then((valid_resp) => {
-      alert(valid_resp)
-    });
+  saveFiles().then(function () {
+    await axios.post('/server/set_reponse.php', { day_num: '4', response: document.getElementById('answer').value, magasin_num: localStorage.getItem('magasin') }, {
+      headers: { 'Content-Type': 'application/json', 'mode': 'cors' }
+    })
+      .then((valid_resp) => {
+        alert(valid_resp)
+      });
+  });
 }
 /* ----------------------------------- REPONSE JEU 5 ----------------------------------- */
 const check_answer5 = async () => {
@@ -51,4 +51,31 @@ const check_answer5 = async () => {
     .then((valid_resp) => {
       alert(valid_resp)
     });
+}
+
+function saveFiles() {
+  var file_data = $('#file-1').prop('files')[0];
+  form_data.append('file', file_data);
+
+  $.ajax({
+    url: 'image.php',
+    dataType: 'text',
+    cache: false,
+    contentType: false,
+    processData: false,
+    data: form_data,
+    type: 'post',
+    success: function (response) {
+      response = JSON.parse(response);
+
+      if (response.error !== undefined) {
+        $("#danger").empty().append('Une erreur s\'est produite').fadeIn().delay(2000).fadeOut();
+        
+        return false;
+      }
+
+      //$('#' + img_url + '_link').attr('href', url);
+      //$("#succes").empty().append(response).fadeIn().delay(2000).fadeOut();
+    }
+  });
 }
