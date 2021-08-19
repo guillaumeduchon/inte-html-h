031b9bdda84a34db894da5013344cd6046a5c598
loryleticee <loryleticee@gmail.com>
2021-08-19 12:40:17 +0200

    fix_vid

--- js/indice.js
+++ js/indice.js
@@ -21,7 +21,7 @@ const fetch_movie = async () => {
             video_number = localStorage.getItem('DAY_NUM');
           }
           console.log("video/video_game_${video_number}",${video_number} )
-          $('.videoreplace').html(`<source src="video/video_game_${video_number}" type="video/mp4">`)
+          $('.videoreplace').html(`<source src="video/video_game_${video_number}.mp4" type="video/mp4">`)
         } else {
           console.log("no day found")
         }
