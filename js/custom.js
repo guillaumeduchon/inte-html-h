cf705dae3f027d92319f0fcd87de3f6cf0a58a8a
loryleticee <loryleticee@gmail.com>
2021-09-03 00:37:14 +0200

    manage response affcihage

--- js/custom.js
+++ js/custom.js
@@ -125,7 +125,7 @@ setTimeout(() => {
             if (Math.abs(heures) == 9 && minutes >= 00) {
               //Do nothing
             } else {
-              DAY_NUM = (Number(DAY_NUM) - 1)
+              //DAY_NUM = (Number(DAY_NUM) - 1)
               localStorage.setItem('DAY_NUM', DAY_NUM)
             }
           }
